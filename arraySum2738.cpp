78f4169868ece2610ae064b8cfc32baafe9f3244
권선 <97091329+gwonseon@users.noreply.github.com>
2023-08-11 14:03:23 +0900

    Add files via upload

---
+++ arraySum2738.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+
+using namespace std;
+
+int a[101][101];
+int b[101][101];
+int m;
+int n;
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cin >> b[i][j];
+		}
+	}
+	for (int i = 1; i <= m; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cout << a[i][j] + b[i][j] << ' ';
+		    
+		}
+		cout << '\n';
+	}
+}
